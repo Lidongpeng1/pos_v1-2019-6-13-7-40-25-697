ea302ca94e4c3ef6b0a128b9d769d7cf35ab0396
“your_uLiDongpeng1 <l>
2019-07-10 22:35:03 +0800

    creatallfunction

--- main/main.js
+++ main/main.js
@@ -9,7 +9,7 @@ function printReceipt(inputs) {
     let discountedCartItems = getDiscountedCartItems(cartItems);
     let receipt = getReceiptFromCartItems(discountedCartItems, sumPrice);
     console.log(receipt);       //不打印则通过不了测试（无输出）
-    return receipt;
+    return receipt;   //总函数，最终返回打印结果
 }
 
 const getCartItemsByInputs = (inputs) => {
@@ -34,3 +34,82 @@ const getCartItemsByInputs = (inputs) => {
     return cartItems;
 };
 
+const getBarcodeFromBarcodeMessage = (barcodeMessage) => {
+    let indexOfMargin = barcodeMessage.indexOf('-');
+    if (indexOfMargin === -1) {
+        return barcodeMessage;
+    }
+    let barcode = barcodeMessage.substring(0, indexOfMargin);
+    return barcode;
+}
+
+const getAmountFromBarcodeMessage = (barcodeMessage) => {
+    let indexofMargin = barcodeMessage.indexOf('-');
+    if (indexofMargin === -1) {
+        return 1;
+    }
+    let amount = parseFloat(barcodeMessage.substring(indexofMargin + 1));
+    return amount;
+}
+
+const indexOfBarcodeInCartItems = (cartItems, barcode) => {
+    for (let index = 0; index < cartItems.length; index++) {
+        if (cartItems[index].barcode == barcode) {
+            return index;
+        }
+    }
+    return -1;                      //查找失败
+}
+
+const getItemFromItemsDB = (barcode) => {
+    const ItemsDB = loadAllItems();
+    for (let i = 0; i < ItemsDB.length; i++) {
+        if (ItemsDB[i].barcode === barcode) {
+            return ItemsDB[i];
+        }
+    }
+    return null;                    //获取item对象失败
+}
+
+const getSumPrice = (cartItems) => {
+    let sumPrice = 0;
+    cartItems.forEach((item) => {
+        sumPrice += item.totalPrice;
+    });
+    return sumPrice;
+}
+
+const getDiscountedCartItems = (cartItems) => {
+    let discountedCartItems = cartItems.map((item) => {
+        if (isBuyTwoGetOneFreeBarcode(item.barcode)) {
+            item.totalPrice -= Math.floor(item.count / 3) * item.price;
+        }
+        return item;
+    });
+    return discountedCartItems;
+}
+
+const isBuyTwoGetOneFreeBarcode = (barcode) => {
+    const promotions = loadPromotions();
+    const buyTwoGetOneFreeBarcodes = promotions[0].barcodes;
+    for (let i = 0; i < buyTwoGetOneFreeBarcodes.length; i++) {
+        if (barcode === buyTwoGetOneFreeBarcodes[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const getReceiptFromCartItems = (discountedCartItems, sumPrice) => {
+    let receipt = '***<没钱赚商店>收据***\n';
+    let discountedSumPrice = 0;
+    discountedCartItems.forEach((item) => {
+        receipt += `名称：${item.name}，数量：${item.count}${item.unit}，单价：${item.price.toFixed(2)}(元)，小计：${item.totalPrice.toFixed(2)}(元)\n`;
+        discountedSumPrice += item.totalPrice;
+    });
+    receipt += `----------------------
+总计：${discountedSumPrice.toFixed(2)}(元)
+节省：${(sumPrice - discountedSumPrice).toFixed(2)}(元)
+**********************`;
+    return receipt;
+}
