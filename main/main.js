64188035d3c7a8e6a95cd360dd211b0f837c0aeb
“your_uLiDongpeng1 <l>
2019-07-10 22:15:10 +0800

    printReceipt and getCartItems

--- main/main.js
+++ main/main.js
@@ -1,4 +1,36 @@
-'use strict';
+﻿'use strict';
 
+function printReceipt(inputs) {
+    let cartItems = getCartItemsByInputs(inputs);
+    if (cartItems === null) {
+        return 'ERROR';
+    }
+    let sumPrice = getSumPrice(cartItems);
+    let discountedCartItems = getDiscountedCartItems(cartItems);
+    let receipt = getReceiptFromCartItems(discountedCartItems, sumPrice);
+    console.log(receipt);       //不打印则通过不了测试（无输出）
+    return receipt;
+}
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+const getCartItemsByInputs = (inputs) => {
+    let cartItems = [];
+    inputs.forEach((barcodeMessage) => {
+        let barcode = getBarcodeFromBarcodeMessage(barcodeMessage);
+        let amount = getAmountFromBarcodeMessage(barcodeMessage);
+        let index = indexOfBarcodeInCartItems(cartItems, barcode);        //获取cartItems中属性值为barcode的item对象的索引
+        if (index === -1) {                      //不存在
+            let item = getItemFromItemsDB(barcode);                       //获取ItemsDB中属性值为barcode的item对象
+            if (item === null) {                 //inputs 内数据有错
+                return null;
+            }
+            item.count = amount;                 //为item对象增加key-value
+            item.totalPrice = item.price * amount;
+            cartItems.push(item);
+        } else {                                 //已存在
+            cartItems[index].count += amount;
+            cartItems[index].totalPrice += cartItems[index].price * amount;
+        }
+    });
+    return cartItems;
+};
+
